737f551609ae25a23ef9359e0c4e48ba826b6aa5
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2020-03-08 17:23:36 +0300

    Update main.py

--- hw_1/main.py
+++ hw_1/main.py
@@ -1,6 +1,6 @@
 def read_file():
     text = ''
-    with open('./in.txt', 'r', encoding='utf8') as f:
+    with open('./out.txt', 'r', encoding='utf8') as f:
         text = ''.join([line for line in f])
     return text
 
@@ -18,11 +18,25 @@ def encrypt_text(text, key):
     return cipher
 
 
+def decrypt_text(text, key):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    cipher = ""
+    for letter in text.lower():
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = (position - key) % len(alphabet)
+            cipher = cipher + alphabet[new_position]
+        else:
+            cipher += letter
+    return cipher
+
+
 def write_file(cipher):
-    with open('./out.txt', 'w', encoding='utf8') as f:
+    with open('./out_2.txt', 'w', encoding='utf8') as f:
         f.write(cipher)
 
 
+x = int(input("Шаг:"))
 text = read_file()
-text = encrypt_text(text, 3)
+text = decrypt_text(text, x)
 write_file(text)
