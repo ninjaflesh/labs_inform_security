3674850f2020de89bc254f4c5bd251cb445f87ae
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2020-04-07 11:46:30 +0300

    Update main.py

--- hw_1/main.py
+++ hw_1/main.py
@@ -1,42 +1,58 @@
-def read_file():
+def read_file_e():
     text = ''
-    with open('./out.txt', 'r', encoding='utf8') as f:
+    with open('./in.txt', 'r', encoding='utf8') as f:
         text = ''.join([line for line in f])
     return text
 
 
 def encrypt_text(text, key):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    cipher = ""
+    ciphere = ""
     for letter in text.lower():
         if letter in alphabet:
             position = alphabet.index(letter)
             new_position = (position + key) % len(alphabet)
-            cipher = cipher + alphabet[new_position]
+            ciphere = ciphere + alphabet[new_position]
         else:
-            cipher += letter
-    return cipher
+            ciphere += letter
+    return ciphere
+
+
+def write_file_e(ciphere):
+    with open('./out.txt', 'w', encoding='utf8') as f:
+        f.write(ciphere)
+
+
+def read_file_d():
+    text = ''
+    with open('./out.txt', 'r', encoding='utf8') as f:
+        text = ''.join([line for line in f])
+    return text
 
 
 def decrypt_text(text, key):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    cipher = ""
+    cipherd = ""
     for letter in text.lower():
         if letter in alphabet:
             position = alphabet.index(letter)
             new_position = (position - key) % len(alphabet)
-            cipher = cipher + alphabet[new_position]
+            cipherd = cipherd + alphabet[new_position]
         else:
-            cipher += letter
-    return cipher
+            cipherd += letter
+    return cipherd
 
 
-def write_file(cipher):
+def write_file_d(cipherd):
     with open('./out_2.txt', 'w', encoding='utf8') as f:
-        f.write(cipher)
+        f.write(cipherd)
 
 
-x = int(input("Шаг:"))
-text = read_file()
-text = decrypt_text(text, x)
-write_file(text)
+key = int(input("Шаг:"))
+text = read_file_e()
+text = encrypt_text(text, key)
+write_file_e(text)
+
+text = read_file_d()
+text = decrypt_text(text, key)
+write_file_d(text)
